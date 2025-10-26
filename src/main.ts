c4b29451d6efe342ca3eec16b9809d27f132dc7b
Heber1709 <heber17castro@gmail.com>
2025-10-26 00:21:04 -0600

    Actualizar main.ts y configuración CORS para producción

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // CORS: FRONTEND LOCAL y GitHub Pages
-  app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:3000',           // desarrollo local
-        'https://heber1709.github.io'      // producción GitHub Pages
-      ];
-      // Si no hay origin (petición desde backend o Postman), permitir
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('CORS no permitido'));
-      }
-    },
-    credentials: true, // si usas cookies o autenticación
-  });
+app.enableCors({
+  origin: (origin, callback) => {
+    if (!origin || /^https:\/\/heber1709\.github\.io/.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS no permitido'));
+    }
+  },
+  credentials: true,
+});
+
+
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
